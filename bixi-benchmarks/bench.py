7cdef41657e517f2f6015d54f5c62f1b331775ce
Piotr Blaszyk <piopio555888@gmail.com>
2024-06-18 07:59:58 +0200

    comments

--- bixi-benchmarks/bench.py
+++ bixi-benchmarks/bench.py
@@ -9,8 +9,8 @@ num_warmup = 0
 num_main = 1
 vendors = [
   'pandas',
-  # 'duckdb',
-  # 'sqlite',
+  'duckdb',
+  'sqlite',
 ]
 
 vendor_input_paths = {
@@ -62,20 +62,13 @@ def predict_duration_from_distance(vendor, table_name):
   go(df)
 
 bixi_queries = {
-  # "data in": data_in,
+  "data in": data_in,
   "predict duration from distance": predict_duration_from_distance,
 }
 
 def load_pandas(table_name):
   path_prefix = vendor_input_paths['pandas']
   res = pd.read_csv(f'{path_prefix}{table_name}.csv')
-  memory_usage_bytes = res.memory_usage(deep=True).sum()
-
-  # Step 2: Convert the memory usage to megabytes
-  memory_usage_mb = memory_usage_bytes / (1024 ** 2)
-
-  print(f"DataFrame size: {memory_usage_mb:.2f} MB")
-
   return res
 
 def load_duckdb(table_name):
@@ -151,5 +144,5 @@ def bench_loop(table_names, queries, results_path):
 
   timings.to_csv(results_path, index=False)
 
-# bench_loop(rand_names, rand_queries, rand_results_path)
+bench_loop(rand_names, rand_queries, rand_results_path)
 bench_loop(bixi_names, bixi_queries, bixi_results_path)
